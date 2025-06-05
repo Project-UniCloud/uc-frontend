4635d4d0c0a318f1cb8dbc05685a94eeb5c01852
Michael <michalluczak4a@gmail.com>
2025-06-05 21:17:57 +0200

    edit url

--- src/lib/utils/baseUrl.js
+++ src/lib/utils/baseUrl.js
@@ -1,6 +1,9 @@
-const baseApiUrl =
-  window.location.hostname === "unicloud.projektstudencki.pl"
-    ? "https://unicloud.projektstudencki.pl/api"
-    : "http://localhost:8080/api";
+export function getBaseApiUrl() {
+  if (typeof window !== "undefined") {
+    return window.location.hostname === "unicloud.projektstudencki.pl"
+      ? "https://unicloud.projektstudencki.pl/api"
+      : "http://localhost:8080/api";
+  }
 
-export default baseApiUrl;
+  return "https://unicloud.projektstudencki.pl/api";
+}
