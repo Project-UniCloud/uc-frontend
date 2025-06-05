1018d5a1ff0179e07b781fb15441d1b97a9db0c5
Michael <michalluczak4a@gmail.com>
2025-06-05 21:00:18 +0200

    update url and dockerfile

--- src/lib/utils/baseUrl.js
+++ src/lib/utils/baseUrl.js
@@ -1,5 +1,6 @@
 const baseApiUrl =
-  process.env.NODE_ENV === "dev"
+  window.location.hostname === "unicloud.projektstudencki.pl"
     ? "https://unicloud.projektstudencki.pl/api"
     : "http://localhost:8080/api";
+
 export default baseApiUrl;
