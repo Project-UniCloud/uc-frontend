e4f13bae38c4bf53fa4bf717ad7db49bd12e9d03
Kosterix0 <ksawerymiszczakgit@gmail.com>
2025-12-04 20:44:50 +0100

    add ResourceTypes

---
+++ src/components/resources/AddResourceTypeModal.jsx
@@ -0,0 +1,117 @@
+import { useRef, useEffect, useState } from "react";
+import { X } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { addResourceType } from "@/lib/resourceApi";
+import InputForm from "../utils/InputForm";
+import { Button } from "../utils/Buttons";
+import { showSuccessToast, showErrorToast } from "../utils/Toast";
+
+export default function AddResourceTypeModal({ isOpen, setIsOpen }) {
+  const dialogRef = useRef(null);
+  const formRef = useRef(null);
+  const [formErrors, setFormErrors] = useState({});
+
+  const mutation = useMutation({
+    mutationFn: (resourceTypeData) => addResourceType(resourceTypeData),
+    onSuccess: () => {
+      setIsOpen(false), setFormErrors({}), formRef.current?.reset();
+      showSuccessToast(
+        "Typ zasobu dodany! Odśwież stronę, aby zobaczyć zmiany."
+      );
+    },
+    onError: (error) => {
+      setFormErrors({ error: error.message || "Błąd dodawania typu zasobu" }),
+        showErrorToast("Błąd dodawania typu zasobu: " + error?.message);
+    },
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [isOpen]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const name = formData.get("name");
+
+    const resourceTypeData = {
+      name,
+    };
+
+    mutation.mutate(resourceTypeData);
+  }
+  function handleClose() {
+    setIsOpen(false);
+    setFormErrors({}), formRef.current?.reset();
+  }
+
+  return (
+    <dialog
+      ref={dialogRef}
+      className="rounded-2xl shadow-xl w-full max-w-lg p-0 m-auto"
+      onClose={handleClose}
+    >
+      <form
+        method="dialog"
+        className="relative p-6"
+        onSubmit={handleSubmit}
+        ref={formRef}
+      >
+        <div className="flex items-start justify-between">
+          <h2 className="text-xl font-semibold mb-6">Dodaj typ zasobu</h2>
+          <button
+            type="button"
+            className={` text-gray-500 hover:text-black cursor-pointer ${
+              mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            onClick={handleClose}
+            disabled={mutation.isPending}
+          >
+            <X />
+          </button>
+        </div>
+        {/* <div className="grid grid-cols-1 gap-4"> */}
+        <InputForm
+          name="name"
+          placeholder="Nazwa typu zasobu"
+          label="Nazwa typu zasobu*"
+          type="text"
+          required
+        />
+        {/* </div> */}
+
+        {formErrors.error && (
+          <div className="text-red-600">{formErrors.error}</div>
+        )}
+
+        <div className="flex justify-end gap-4 pt-4">
+          <Button
+            type="button"
+            onClick={handleClose}
+            color="bg-white"
+            textColor="text-black"
+            disabled={mutation.isPending}
+            className={`border border-black ${
+              mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
+            Anuluj
+          </Button>
+          <Button
+            type="submit"
+            disabled={mutation.isPending}
+            className={`${
+              mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
+            {mutation.isPending ? "Dodawanie..." : "Dodaj typ zasobu"}
+          </Button>
+        </div>
+      </form>
+    </dialog>
+  );
+}
---
+++ src/components/resources/DeleteResourceTypeModal.jsx
@@ -0,0 +1,98 @@
+import { useRef, useEffect, useState } from "react";
+import { X } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "../utils/Buttons";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { deleteResourceType } from "@/lib/resourceApi";
+
+export default function DeleteResourceTypeModal({
+  isOpen,
+  setIsOpen,
+  resourceTypeId,
+  setSelectedResourceTypeId,
+  cloudConnectorId,
+}) {
+  const dialogRef = useRef(null);
+  const [errors, setErrors] = useState({});
+
+  const mutation = useMutation({
+    mutationFn: (resourceTypeId, cloudConnectorId) =>
+      deleteResourceType(resourceTypeId, cloudConnectorId),
+    onSuccess: () => {
+      setIsOpen(false), setErrors({}), formRef.current?.reset();
+      setSelectedResourceTypeId(null);
+      showSuccessToast(
+        "Typ zasobu usunięty! Odśwież stronę, aby zobaczyć zmiany."
+      );
+    },
+    onError: (error) => {
+      setErrors({ error: error.message || "Błąd usuwania typu zasobu" }),
+        showErrorToast("Błąd usuwania typu zasobu: " + error?.message);
+      setSelectedResourceTypeId(null);
+    },
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [isOpen]);
+
+  function handleClose() {
+    setIsOpen(false);
+    setErrors({});
+    setSelectedResourceTypeId(null);
+  }
+
+  return (
+    <dialog
+      ref={dialogRef}
+      className="rounded-2xl shadow-xl w-full max-w-lg p-6 m-auto"
+      onClose={handleClose}
+    >
+      <div className="flex flex-col items-end">
+        <button
+          type="button"
+          className=" text-gray-500 hover:text-black cursor-pointer"
+          onClick={handleClose}
+        >
+          <X />
+        </button>
+      </div>
+      <h2 className="text-xl font-semibold mb-10 text-center">
+        Usuń typ zasobu
+      </h2>
+      <p className="text-gray-600 mb-6 text-center text-xl ">
+        Czy jesteś pewny, że chcesz usunąć ten typ zasobu?
+      </p>
+      {errors.error && <div className="text-red-600">{errors.error}</div>}
+      <div className="flex justify-end items-center gap-4 pt-10">
+        <Button
+          type="button"
+          onClick={handleClose}
+          color="bg-white"
+          textColor="text-black"
+          disabled={mutation.isPending}
+          className={`border border-black ${
+            mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          Anuluj
+        </Button>
+        <Button
+          color="bg-red-600"
+          type="submit"
+          className={`${
+            mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          onClick={() => mutation.mutate(resourceTypeId, cloudConnectorId)}
+        >
+          {mutation.isLoading ? "Usuwanie..." : "Usuń"}
+          <FaRegTrashAlt />
+        </Button>
+      </div>
+    </dialog>
+  );
+}
